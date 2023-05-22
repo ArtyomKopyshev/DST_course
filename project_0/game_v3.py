05dffda466b0c3427488f45b3d771435ade57cb8
ArtyomKopyshev <artyom.a.kopyshev@gmail.com>
2023-05-22 23:56:49 +0300

    edit readme.md

--- project_0/game_v3.py
+++ project_0/game_v3.py
@@ -1,5 +1,6 @@
 import numpy as np
 
+
 def game_core(number: int = 1) -> int:
     """Функция угадывания числа, гарантированно отгадывает число за 7 попыток.
        Алгоритм угадывания основан на методе дихотомии (половинного деления).
@@ -10,8 +11,8 @@ def game_core(number: int = 1) -> int:
     Returns:
         int: Число попыток
     """
-    count, predict = 1, 50
-    upper_border, lower_border = 101, 0
+    count, predict = 1, 50 #Cчётчик попыток и предполагаемое число
+    upper_border, lower_border = 101, 0 #Верхняя и нижняя граница угадывания
     
     while number != predict:
         count += 1     
@@ -23,6 +24,7 @@ def game_core(number: int = 1) -> int:
             predict -= (predict - lower_border)//2
 
     return count
+
 
 def score_game(random_predict) -> int:
     """За какое количество попыток в среднем за 10000 подходов угадывает наш алгоритм
@@ -37,14 +39,15 @@ def score_game(random_predict) -> int:
     np.random.seed(1)  # фиксируем сид для воспроизводимости
     random_array = np.random.randint(1, 101, size=(10000))  # загадали список чисел
 
-    for number in random_array:
+    for number in random_array: #Угадываем каждое число из списка
         count_ls.append(random_predict(number))
-
+        
     score = int(np.mean(count_ls)) # Среднее число попыток
     max_score = int(max(count_ls)) # Максимальное число попыток
     min_score = int(min(count_ls)) # Минимальное число попыток
-    print(f"Ваш алгоритм угадывает число в среднем за: {score} попыток, макмимальное кол-во попыток: {max_score}, минимальное количество попыток: {min_score}")
+    print(f"Ваш алгоритм угадывает число в среднем за: {score} попыток, максимальное кол-во попыток: {max_score}, минимальное количество попыток: {min_score}")
     return score
     
-    
-score_game(game_core)
+
+if __name__ == '__main__':    
+    score_game(game_core)
