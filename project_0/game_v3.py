07652d7579732e4c78f8339ba35ceb3d22969dec
ArtyomKopyshev <artyom.a.kopyshev@gmail.com>
2023-05-23 00:58:14 +0300

    final

--- project_0/game_v3.py
+++ project_0/game_v3.py
@@ -47,6 +47,7 @@ def score_game(random_predict) -> int:
     min_score = int(min(count_ls)) # Минимальное число попыток
     print(f"Ваш алгоритм угадывает число в среднем за: {score} попыток, максимальное кол-во попыток: {max_score}, минимальное количество попыток: {min_score}")
     #return score
+  
     
 if __name__ == '__main__':
     score_game(game_core_v3)
