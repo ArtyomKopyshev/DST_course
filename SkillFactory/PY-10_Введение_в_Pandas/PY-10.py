c6bd8c8576266aacfe2bac01d661cb5c5335f2b1
ArtyomKopyshev <artyom.a.kopyshev@gmail.com>
2023-06-14 23:23:36 +0300

    new_skillfactory

--- SkillFactory/PY-10_Введение_в_Pandas/PY-10.py
+++ SkillFactory/PY-10_Введение_в_Pandas/PY-10.py
@@ -1,4 +1,15 @@
 import pandas as pd
 
-melb_data = pd.read_csv('data/melb_data.csv', sep=',')
-print(melb_data.loc[15, 'Price'])
+melb_data = pd.read_csv('D:\IDE\SkillFactory\PY-10_Введение_в_Pandas\data\melb_data.csv', sep=',')
+print(melb_data.loc[3521, 'Landsize']/melb_data.loc[1690, 'Landsize'])
+#print(melb_data.tail(7))
+#print(melb_data.shape)
+print(melb_data.info())
+melb_data['Car'] = melb_data['Car'].astype('int64')
+melb_data['Bedroom'] = melb_data['Bedroom'].astype('int64')
+melb_data['Bathroom'] = melb_data['Bathroom'].astype('int64')
+melb_data['Propertycount'] = melb_data['Propertycount'].astype('int64')
+melb_data['YearBuilt'] = melb_data['YearBuilt'].astype('int64')
+#print(melb_data.info())
+#print(melb_data.describe().loc[:, ['Distance', 'BuildingArea' , 'Price']])
+print(melb_data['Type'].value_counts(normalize=True))
